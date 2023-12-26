51dca7a72d8bb436a46821d50479d8eb907ad52b
JB <drv98@naver.com>
2023-12-26 12:17:28 +0900

    유저 rest컨트롤러

---
+++ src/main/java/com/mysite/backend/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.mysite.backend.controller;
+
+import com.mysite.backend.model.User;
+import com.mysite.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+}

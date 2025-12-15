3691208e0c269d3698f51ea10b008b6cc35481e2
KJB <drv98@naver.com>
2025-12-15 10:10:29 +0900

    컨트롤러

---
+++ src/main/java/com/mysite/backend/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.mysite.backend.controller;
+
+import com.mysite.backend.model.User;
+import com.mysite.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/users")
+    User createUser(@RequestBody User user){
+        return userRepository.save(user);
+    }
+}

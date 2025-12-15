0ea10c072689d060bdd5fc0d810a007d373ad3e6
KJB <drv98@naver.com>
2025-12-15 15:25:30 +0900

    유저 업데이트

--- src/main/java/com/mysite/backend/controller/UserController.java
+++ src/main/java/com/mysite/backend/controller/UserController.java
@@ -30,4 +30,17 @@ public class UserController {
         return userRepository.findById(id)
                 .orElseThrow(()->new UserNotFoundException(id));
     }
+
+    @PutMapping("/users/{id}")
+    User updateUser(@PathVariable Long id, @RequestBody User newUser){
+        //업데이트는 일단 id로 유저를 찾은 뒤 업데이트 함 없을경우 예외처리도 함
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                })
+                .orElseThrow(()->new UserNotFoundException(id));
+    }
 }

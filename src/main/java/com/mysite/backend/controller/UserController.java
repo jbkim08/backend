691fdf719adb37ec114c840850051ccdc79bfdd6
KJB <drv98@naver.com>
2025-12-15 11:49:18 +0900

    cors 동일출처 에러 해결

--- src/main/java/com/mysite/backend/controller/UserController.java
+++ src/main/java/com/mysite/backend/controller/UserController.java
@@ -3,17 +3,23 @@ package com.mysite.backend.controller;
 import com.mysite.backend.model.User;
 import com.mysite.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
     @PostMapping("/users")
-    User createUser(@RequestBody User user){
-        return userRepository.save(user);
+    User createUser(@RequestBody User user){ //클라이언트에서 json객체를 서버로 보냄 => User객체로 변환
+        return userRepository.save(user); //유저를 저장하고 다시 리턴
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll(); //db의 모든 유저를 리스트로 리턴
     }
 }
